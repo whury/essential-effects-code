49ba32c10f2009f7d35f4603463485f13803a1b5
Wojtek Hury <wojtek.hury@gmail.com>
2023-10-22 21:03:24 +0200

    solve exercise 2

--- exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
+++ exercises/src/main/scala/com.innerproduct.ee/io/TickingClock.scala
@@ -1,10 +1,15 @@
 package com.innerproduct.ee.io
 
 import cats.effect._
+import scala.concurrent.duration.{ FiniteDuration, SECONDS }
 
 object TickingClock extends IOApp.Simple {
   def run: IO[Unit] =
     tickingClock
 
-  val tickingClock: IO[Unit] = ??? // <1>
+  val tickingClock: IO[Unit] = for {
+    _ <- IO(println(System.currentTimeMillis()))
+    _ <- IO.sleep(FiniteDuration(1, SECONDS))
+    _ <- tickingClock
+  } yield ()
 }
