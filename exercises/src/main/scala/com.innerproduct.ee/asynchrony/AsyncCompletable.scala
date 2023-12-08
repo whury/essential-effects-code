3e371dd816459bc85f8081c66a65e80c9b13015b
Wojtek Hury <wojtek.hury@gmail.com>
2023-12-08 18:26:18 +0100

    solve exercise 4

--- exercises/src/main/scala/com.innerproduct.ee/asynchrony/AsyncCompletable.scala
+++ exercises/src/main/scala/com.innerproduct.ee/asynchrony/AsyncCompletable.scala
@@ -1,6 +1,7 @@
 package com.innerproduct.ee.asynchrony
 
 import cats.effect._
+import com.innerproduct.ee.debug._
 import java.util.concurrent.CompletableFuture
 import scala.jdk.FunctionConverters._
 
@@ -14,7 +15,13 @@ object AsyncCompletable extends IOApp.Simple {
   def fromCF[A](cfa: IO[CompletableFuture[A]]): IO[A] =
     cfa.flatMap { fa =>
       IO.async_ { cb =>
-        val handler: (A, Throwable) => Unit = ??? // <1>
+        val handler: (A, Throwable) => Unit = (result, error) => {
+          if (error != null) {
+            cb(Left(error))
+          } else {
+            cb(Right(result))
+          }
+        }
 
         fa.handle(handler.asJavaBiFunction) // <2>
 
